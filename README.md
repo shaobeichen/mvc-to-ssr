84ce855d6f47d084575ff234e5bdb205d53d1e64
LeachZhou <leachzhou@163.com>
2022-02-16 16:39:42 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # mvc-to-ssr
-一个依次从 MVC、SPA、SSR 进程演变的案例
+
+> 一个依次从 MVC、SPA、SSR 进程演变的案例
+
+## 版本
+
+### mvc 版本
+
+- mvc 版本是 MVC 的渲染逻辑，
+- 主要逻辑：简单来说 html 负责样式，后端负责数据逻辑。例如 PHP 模板渲染
+- 部署：非静态页面部署
+- 缺点：前后端不分离，开发效率低。
+
+### spa 版本
+
+- spa 版本是 MMVM 客户端渲染逻辑，
+- 主要逻辑：html 负责样式、数据逻辑，后端负责给数据。例如 Vue 单页面
+- 部署：静态页面部署
+- 缺点：单页面，seo 不友好，渲染速度有待优化。
+
+### ssr 版本
+
+- ssr 版本是 MVVM 服务端渲染逻辑，
+- 主要逻辑：html 负责样式、数据逻辑，后端负责给数据。例如 Nuxt 服务端渲染
+- 部署：非静态页面部署
+- **优点**：前后端分离，seo 友好，渲染速度快。
+
+## 使用
+
+### mvc 使用
+
+`npm i & npm run dev`
+
+### spa 使用
+
+vscode 打开 index.html，右键 Open In Default Brower
+
+### ssr 使用
+
+`npm i & npm run dev`
+
+## 参考内容
+
+https://github.com/wmui/vue-ssr-demo
---
+++ mvc/server/index.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const server = express()
+
+server.get('*', (req, res) => {
+  renderToString(
+    {
+      data: {
+        content: '',
+        image: '',
+      },
+      handle: async (data) => {
+        await sleep(0)
+        data.content = '第一个标题，第一个描述'
+        data.image = 'https://sponsors.vuejs.org/images/nuxt.avif'
+        return data
+      },
+      template: fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf-8'), // 服务端渲染数据
+    },
+    (html) => res.end(html),
+  )
+})
+
+server.listen(8010, () => {
+  console.log('访问中：http://127.0.0.1:8010')
+})
+
+const sleep = (delay) => {
+  return new Promise((resolve) => setTimeout(resolve, delay))
+}
+/**
+ * 双括号替换值
+ * @example
+ * const str="姓名：{{name}},性别：{{gender}},年龄：{{age}}"
+ * const data={name:"小红",gender:"女",age:2}
+ * console.log(renderReplace(str,data)) // 姓名：小红,性别：女,年龄：2
+ */
+const renderReplace = (str, data) => {
+  const re = /{{([^}]+)?}}/g
+  return str.replace(re, ($0, $1, $2) => {
+    if ($1 in data) return data[$1]
+  })
+}
+
+const renderToString = async (options, cb) => {
+  const { template, data, handle } = options
+  const newData = await handle(data)
+  const html = renderReplace(template, newData)
+  cb(html)
+}
